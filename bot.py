63809383c289d5bd1d7f27a88a012eb49c00aa00
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2021-08-24 12:35:04 -0700

    uses chrome, not multi-process

--- bot.py
+++ bot.py
@@ -9,92 +9,98 @@ import links
 import os
 from datetime import datetime
 
-# initialize webbrowser "driver" object
-opts = ChromeOptions()
-driver = webdriver.Chrome(options=opts)
+def bot():
+    # get PID
+    PID = os.getpid()
 
-# load best buy store page for items
-driver.get(links.XBOX_SX1)
+    # initialize webbrowser "driver" object
+    opts = ChromeOptions()
+    driver = webdriver.Chrome(options=opts)
 
-# default wait mechanism for all elements
-wait = WebDriverWait(driver, 10, poll_frequency=1)
+    # load best buy store page for items
+    driver.get(links.XBOX_SX1)
 
-# variable to store if script has completed or not
-# variable is updated based on whether a file has been created in the directory of the script
-# the file will be named DONE
-isComplete = False
-if os.path.exists('./DONE'):
-    isComplete = True
+    # default wait mechanism for all elements
+    wait = WebDriverWait(driver, 10, poll_frequency=1)
 
-# keep track of when script starts
-time_start = datetime.now()
-
-while not isComplete:
-    # find add to cart button
-    try:
-        atcBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-primary.btn-lg.btn-block.btn-leading-ficon.add-to-cart-button')))
-    except:
-        driver.refresh()
-        continue
-
-    print('Added item to cart')
-
-    try:
-        print(atcBtn)
-
-        # add to cart
-        atcBtn.click()
-        print('added item to cart')
-
-        # go to cart and begin checkout as guest
-        driver.get('https://www.bestbuy.com/cart')
-        print('navigated to cart')
-
-        # click checkout button
-        checkoutBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-lg.btn-block.btn-primary')))
-        checkoutBtn.click()
-        print('navigating to checkout')
-
-        # fill in email
-        emailField = wait.until(EC.element_to_be_clickable((By.ID, 'fld-e')))
-        emailField.send_keys(info.email)
-        print('entered email')
-
-        # fill in password and press enter
-        pwdField = wait.until(EC.element_to_be_clickable((By.ID, 'fld-p1')))
-        pwdField.send_keys(info.password)
-        pwdField.send_keys(Keys.ENTER)
-        print('entered password')
-
-        # switch to shipping if its not selected
-        shippingLink = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.ispu-card__switch')))
-        if shippingLink.text == 'Switch to Shipping':
-            shippingLink.click()
-            print('switched from pickup to shipping')
-        else:
-            print('shipping already selected')
-
-        # fill in card cvv
-        cvvField = wait.until(EC.element_to_be_clickable((By.ID, 'cvv')))
-        cvvField.send_keys(info.cvv)
-        print('entered cvv')
-
-        # place order
-        # WARNING: uncommenting the code below will allow the bot to make purchases autonomously!!!
-        # placeOrderBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.button__fast-track')))
-        # placeOrderBtn.click()
-
-        # make the file: DONE in the local directory, which tells the script to stop running
-        open('./DONE', 'w')
+    # variable to store if script has completed or not
+    # variable is updated based on whether a file has been created in the directory of the script
+    # the file will be named DONE
+    isComplete = False
+    if os.path.exists('./DONE'):
         isComplete = True
-    except:
-        # make sure this link is the same as the link passed to driver.get() before looping
-        driver.get(links.XBOX_SX1)
-        print(f'Error - restarting bot\n')
-        continue
 
-# get finish time of script
-time_end = datetime.now()
-print('Order successfully placed')
-print(f'total time: {time_end - time_start}')
-driver.close()
+    # keep track of when script starts
+    time_start = datetime.now()
+
+    while not isComplete:
+        # find add to cart button
+        try:
+            atcBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-primary.btn-lg.btn-block.btn-leading-ficon.add-to-cart-button')))
+        except:
+            driver.refresh()
+            continue
+
+        try:
+            # add to cart
+            atcBtn.click()
+            print(f'PID: {PID} added item to cart')
+
+            # go to cart and begin checkout as guest
+            driver.get('https://www.bestbuy.com/cart')
+            print(f'PID: {PID} navigated to cart')
+
+            # click checkout button
+            checkoutBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-lg.btn-block.btn-primary')))
+            checkoutBtn.click()
+            print(f'PID: {PID} navigating to checkout')
+
+            # fill in email
+            emailField = wait.until(EC.element_to_be_clickable((By.ID, 'fld-e')))
+            emailField.send_keys(info.email)
+            print(f'PID: {PID} entered email')
+
+            # fill in password and press enter
+            pwdField = wait.until(EC.element_to_be_clickable((By.ID, 'fld-p1')))
+            pwdField.send_keys(info.password)
+            pwdField.send_keys(Keys.ENTER)
+            print(f'PID: {PID} entered password')
+
+            # switch to shipping if its not selected
+            shippingLink = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.ispu-card__switch')))
+            if shippingLink.text == 'Switch to Shipping':
+                shippingLink.click()
+                print(f'PID: {PID} switched from pickup to shipping')
+            else:
+                print(f'PID: {PID} shipping already selected')
+
+            # fill in card cvv
+            cvvField = wait.until(EC.element_to_be_clickable((By.ID, 'cvv')))
+            cvvField.send_keys(info.cvv)
+            print(f'PID: {PID} entered cvv')
+
+            # place order
+            # WARNING: uncommenting the code below will allow the bot to make purchases autonomously!!!
+            # placeOrderBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.button__fast-track')))
+            # placeOrderBtn.click()
+
+            # make the file: DONE in the local directory, which tells the script to stop running
+            open('./DONE', 'w')
+            isComplete = True
+        except:
+            # make sure this link is the same as the link passed to driver.get() before looping
+            driver.get(links.XBOX_SX1)
+            print(f'Error - restarting bot\n')
+            continue
+
+    # get finish time of script
+    time_end = datetime.now()
+    print(f'PID: {PID} Order successfully placed')
+    print(f'total time: {time_end - time_start}')
+    driver.close()
+
+cwd = os.getcwd()
+process_dir = f'{cwd}/BestBuyBot - {datetime.now()}'
+os.mkdir(process_dir)
+os.chdir(process_dir)
+bot()
--- links.py
+++ links.py
@@ -1,1 +1,2 @@
 XBOX_SX1 = "https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324"
+# test = 'https://www.bestbuy.com/site/pny-elite-turbo-attache-4-64gb-usb-3-0-type-a-flash-drive-black-gray/1876003.p?skuId=1876003'
